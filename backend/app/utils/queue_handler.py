28868c4f09b16fb35a2237f0dcddaf56cc364e14
donpaul <donpaul.oduordonpaul@gmail.com>
2024-11-20 20:32:51 +0300

    updated models to add Mesaage table for storing notifications and modified otp_notification_callback to save every published message to the db

--- backend/app/utils/queue_handler.py
+++ backend/app/utils/queue_handler.py
@@ -1,10 +1,11 @@
 
 import json
 import pika.exceptions
-from app import create_app
+from app import create_app, db
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from kombu import Connection, Exchange, Queue
+from app.models import  Message
 
 app = create_app()
 CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
@@ -33,15 +34,26 @@ def publish_otp(otp, phone_number):
 
 #Deserialize the message to obj:
 def otp_notification_callback(body, message):
-    notification = json.loads(body)
-    otp = notification.get('otp')
-    phone_number = notification.get('phone_number')
+    with app.app_context():
+        notification = json.loads(body)
+        otp = notification.get('otp')
+        phone_number = notification.get('phone_number')
 
-    print(f"Recieved OTP: {otp} for phone number {phone_number}")
+        print(f"Recieved OTP: {otp} for phone number {phone_number}")
 
-    #Send OTP data to frontend
-    socketio.emit('otp_notification', {'otp': otp, 'phone_number': phone_number})
-    message.ack()
+        #Save the message to the database
+        try:
+            new_message = Message(otp=otp, phone_number=phone_number)
+            db.session.add(new_message)
+            db.session.commit()
+            print(f"Message saved to the Database: {new_message}")
+        except Exception as e:
+            print(f"Error saving message: {e}")
+            db.session.rollback()
+
+        #Send OTP data to frontend
+        socketio.emit('otp_notification', {'otp': otp, 'phone_number': phone_number})
+        message.ack()
 
 #Start rabbit mq consumer
 def start_consumer():
